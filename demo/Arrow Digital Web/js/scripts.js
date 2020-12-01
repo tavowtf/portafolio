ed9b1d393cce2ba1ab2a41ec50cf7685b6a14f7a
Gustavo Avila P <gustavoavilap@192.168.0.11>
2020-11-30 21:48:18 -0500

    segundo commit

---
+++ demo/Arrow Digital Web/js/scripts.js
@@ -0,0 +1,9 @@
+/*----------------------------------
+Iniciamos smoothScroll (Scroll Suave)
+--------------------------------*/
+smoothScroll.init({
+    speed: 1600, // Integer. How fast to complete the scroll in milliseconds
+    offset: 60, // Integer. How far to offset the scrolling anchor location in pixels
+
+});
+
